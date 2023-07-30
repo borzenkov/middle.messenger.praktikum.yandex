0e92bf804afcceae1bcbf66100669a9270b0e502
Viktor Borzenkov <vit-borzenkov@yandex.ru>
2023-07-30 16:59:13 +0300

    проба handlebars

--- chat/vite.config.js
+++ chat/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [handlebars()],
+    plugins: [handlebars({
+        partialDirectory: resolve(__dirname, 'src/partials'),
+        context: {
+            username: 'Виктор'
+        }
+    })],
 })
