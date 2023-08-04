68fd8efd194abe4bba77769477f94297267a7505
Viktor Borzenkov <vit-borzenkov@yandex.ru>
2023-08-04 22:02:24 +0300

    финальные штрихи

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,16 @@ import { resolve } from 'path';
 export default defineConfig({
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'src/partials'),
+        context: {
+            user: {
+                email: 'email@yandex.ru',
+                login: 'anonymous',
+                firstName: 'Аноним',
+                secondName: 'Анонимов',
+                displayName: '(V)O_O(V)',
+                phone: '88005553535'
+            }
+        }
     })],
     build: {
         rollupOptions: {
@@ -15,6 +25,10 @@ export default defineConfig({
                 profile: resolve(__dirname, 'src/pages/profile/profile.html'),
                 changeProfile: resolve(__dirname, 'src/pages/profile/edit-profile.html'),
                 changePassword: resolve(__dirname, 'src/pages/profile/edit-password.html'),
+                chats: resolve(__dirname, 'src/pages/chats/chats.html'),
+                fourHundred: resolve(__dirname, 'src/pages/errors/404.html'),
+                fiveHundred: resolve(__dirname, 'src/pages/errors/500.html'),
+                uploadAvatar: resolve(__dirname, 'src/pages/profile/upload-profile-avatar.html'),
             },
         },
     },
